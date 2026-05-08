e520e95e35af776e0b9dbf51776e5dfb52f09714
HAYALETBEY437 <60708954+HAYALETBEY437@users.noreply.github.com>
2026-05-08 13:11:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import os
 app = Flask(__name__)
 
 # --- AYARLAR ---
-# Serveo'nun verdiği uzun adresi buraya yapıştır (HTTPS kısmını sil)
-TUNNEL_URL = "ddbd18f71e953411-34-6-91-209.serveousercontent.com"
-TUNNEL_PORT = 80
+# Pinggy'nin verdiği adresi buraya yapıştır (http:// kısmını sil)
+# Örnek: xuhba-34-6-91-209.run.pinggy-free.link
+TUNNEL_URL = "xuhba-34-6-91-209.run.pinggy-free.link" 
+TUNNEL_PORT = 10101
 # ---------------
 
 @app.route('/')
 def index():
+    # Botlar bu adrese gelip TUNNEL_URL ve PORT bilgisini okuyacak
     return f"{TUNNEL_URL}:{TUNNEL_PORT}"
 
 if __name__ == "__main__":
+    # Render'ın kendi portu (5000 veya otomatik atanan)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
