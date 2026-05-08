d5ee050fdac9ed1ff5b8800d3ddd76824f7288c0
HAYALETBEY437 <60708954+HAYALETBEY437@users.noreply.github.com>
2026-05-08 12:46:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,15 @@ import os
 app = Flask(__name__)
 
 # --- AYARLAR ---
-# Localtunnel'ın verdiği adresi buraya sabitledik
-TUNNEL_URL = "forty-kiwis-cross.loca.lt"
+# Serveo'nun verdiği uzun adresi buraya yapıştır (HTTPS kısmını sil)
+TUNNEL_URL = "a581dbde67190beb-34-6-91-209.serveousercontent.com"
 TUNNEL_PORT = 80
 # ---------------
 
 @app.route('/')
 def index():
-    # Botlar Render'a geldiğinde bu adresi okuyup senin panele akacaklar
     return f"{TUNNEL_URL}:{TUNNEL_PORT}"
 
 if __name__ == "__main__":
-    # Render'ın otomatik atadığı portu kullanır
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
